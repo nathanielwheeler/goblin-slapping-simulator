7fdbe85fbd653170d2475efee788c526a11e3a75
Nathaniel Wheeler <contact@nathanielwheeler.com>
2019-08-16 16:02:08 -0600

    refactor

--- game.js
+++ game.js
@@ -1,11 +1,39 @@
-let goblin = {
-    name: "gerblin",
-    health: 1000000000,
+let player = {
+    name: "Slapmaster, master of slaps",
+    health: 1000,
+    attacks: {
+        slap: 20,
+        backslap: 30,
+        megaslap: 100,
+    },
+    items: {
+        cheekguards: {
+            name: "Cheek Guards",
+            modBase: 5,
+            modType: "enemy.attack",
+            description: "Little pillows that tie onto your cheeks.  Guards against slap attacks."
+        },
+        palmOil: {
+            name: "Palm Oil",
+            modBase: 1.5,
+            modType: "player.attacks"
+        },
+    }
+
 }
-function slap() {
-    goblin.health--
+let enemy = {
+    name: "Tenderpalm, weak of wrist",
+    health: 200,
+    attack: 10
+}
+
+function slap(slapType) {
+
+    goblin.health -= player.attacks[slapType]
+    player.health -= goblin.attack
     draw()
 }
 function draw() {
-    document.querySelector("#health").textContent = `helth: ${goblin.health}`
+    document.querySelector("#goblin-health").textContent = `helth: ${goblin.health}`
+    document.querySelector("#player-health").textContent = `you're helth: ${player.health}`
 }
